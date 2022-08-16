00c51858ffc143bd377fa25828a8415a3d2e9ac2
Maria Laura Altamirano <laltamirano@redsos.com.ar>
2022-08-16 16:49:55 -0300

    error localstorage

--- JS/registros.js
+++ JS/registros.js
@@ -1,9 +1,3 @@
-loguin = localStorage.getItem("inicio-sesion")
-boton = document.getElementById('btn_enviar')
-
-
-!loguin && capturarDatosSesion();
-
 function capturarDatosSesion(){
     
     let emailSesion = document.getElementById("email_sesion").value;
@@ -20,21 +14,16 @@ function capturarDatosRegistro(){
 
     let nombreRegistro = document.getElementById("nombre_registro").value;
     let emailRegistro = document.getElementById("email_registro").value;
-    let usuarioRegistro = document.getElementById("usuario_registro").value;
     let passRegistro = document.getElementById("pass_registro").value;
 
     nombreRegistro = document.getElementById("nombre_registro").value;
     emailRegistro = document.getElementById("email_registro").value;
-    usuarioRegistro = document.getElementById("usuario_registro").value;
     passRegistro = document.getElementById("pass_registro").value;
     
     localStorage.setItem("nombre_registro", nombreRegistro);
     localStorage.setItem("email_registro", emailRegistro);
-    localStorage.setItem("usuario_registro", usuarioRegistro);
     localStorage.setItem("pass_registro", passRegistro);
 }
-
- 
 
 
 
